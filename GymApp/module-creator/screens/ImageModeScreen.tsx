f80dc34595ebf33f59ac1dd6873b26afea96d2de
matCh00 <mateusz.chalik.g@gmail.com>
2022-08-29 00:01:56 +0200

    rozdzielenie dwóch obrazków wyboru partii ciała na dwa osobne komponenty, dodanie funkcjonalności przełączania sie pomiędzy nimi, wystylowanie jednego z tych komponentów

--- GymApp/module-creator/screens/ImageModeScreen.tsx
+++ GymApp/module-creator/screens/ImageModeScreen.tsx
@@ -2,15 +2,21 @@
  * Ekran kreatora planów treningowych - obrazek
  */
 
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import useTheme from '../../theme/hooks/useTheme';
 import useThemedStyles from '../../theme/hooks/useThemeStyles';
 import { ThemeModel } from '../../theme/models/ThemeModel';
 import BackgroundTemplate from '../../shared/components/BackgroundTemplate';
 import { GlobalStyles } from '../../theme/utils/GlobalStyles';
-import ImageMode from '../components/ImageMode';
+import BodyPartsBack from '../components/BodyPartsBack';
+import BodyPartsFront from '../components/BodyPartsFront';
+import { useState } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import OwnButton from '../../shared/components/OwnButton';
 
 const ImageModeScreen = () => {
+
+  const [isFront, setIsFront] = useState(true);
 
   /**
    * motyw
@@ -21,8 +27,18 @@ const ImageModeScreen = () => {
   return (
     <BackgroundTemplate>
       <View style={GlobalStyles.container}>
-        <ImageMode />
+
+        {isFront
+        ? <BodyPartsFront />
+        : <BodyPartsBack />
+        }
+
       </View>
+
+      <View style={{marginBottom: 10, marginTop: -30}}>
+        <OwnButton title="swap" onPress={() => {setIsFront((value) => !value)}} />
+      </View>
+
     </BackgroundTemplate>
   );
 };
@@ -30,4 +46,9 @@ const ImageModeScreen = () => {
 export default ImageModeScreen;
 
 const styles = (theme: ThemeModel) =>
-  StyleSheet.create({});
+  StyleSheet.create({
+    swap: {
+      marginBottom: -80,
+      left: '-40%',
+    }
+  });
